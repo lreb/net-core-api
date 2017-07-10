a5ffe10266ecdfe6c4c04528c7c7706ab299032a
Luis Raul Espinoza Barboza <luis.espinoza@buildbinder.com>
2017-07-10 14:23:46 -0600

    created context

---
+++ core/Models/CoreContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+namespace core.Models
+{
+    public class CoreContext : DbContext
+    {
+        public CoreContext(DbContextOptions<CoreContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
